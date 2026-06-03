0f6d19aa9c920e74f53ad2d2627bde40c0c6b6ca
vigno0405 <lorenzo.vignoli0405@gmail.com>
2026-06-03 14:28:00 +0200

    Update 2026-06-03

--- PassiveCompliance/Hand/HelperPianoMIDI/piano_config.py
+++ PassiveCompliance/Hand/HelperPianoMIDI/piano_config.py
@@ -24,7 +24,7 @@ UR5_INIT_ACCEL = 0.05    # [m/s²]
 PRESS_POSE              = np.deg2rad([80.0, 80.0, 80.0])  # [MCP, PIP, DIP] flexion [rad]
 SPREAD_ANGLE_DEG        = 10.0           # [deg]  finger abduction/adduction
 PIANO_FINGERS_PLAYING   = ['index', 'ring']
-PIANO_FINGERS_GLISSANDO = ['index', 'middle']
+PIANO_FINGERS_GLISSANDO = ['middle']
 
 K_ROT       = 0.4    # [N·m/rad]  background (non-playing fingers)
 K_ROT_PRESS = 0.05   # [N·m/rad]  playing fingers, PIP/DIP (task spring takes over)
