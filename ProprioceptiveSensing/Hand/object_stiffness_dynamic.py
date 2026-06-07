48b5b243e2a955335ae43cb637b78806c447b8cd
vigno0405 <lorenzo.vignoli0405@gmail.com>
2026-06-07 11:56:57 +0200

    Update 2026-06-07

--- ProprioceptiveSensing/Hand/object_stiffness_dynamic.py
+++ ProprioceptiveSensing/Hand/object_stiffness_dynamic.py
@@ -115,10 +115,28 @@ K_JOINT_DICT_MODEL = {
 # =============================================================================
 # Object / ramp-condition sequencing
 # =============================================================================
-_obj_idx          = 0
-_run_idx          = 0
-_ramp_dur_idx     = 0
-_n_runs           = N_RUNS if not COLLECTED_DATA else 1
+_obj_idx      = 0
+_ramp_dur_idx = 0
+_n_runs       = N_RUNS if not COLLECTED_DATA else 1
+
+def _find_resume_run():
+    if _n_runs <= 1:
+        return 0
+    folder = os.path.join(_HERE, 'outputs', 'object_stiffness_dynamic')
+    if not os.path.isdir(folder):
+        return 0
+    for run in range(_n_runs - 1, -1, -1):
+        suffix = f'_run{run + 1}'
+        if all(
+            os.path.isfile(os.path.join(folder,
+                f'object_stiffness_dynamic_{obj}_dur{int(round(dur * 1000))}ms{suffix}.csv'))
+            for obj in OBJECTS
+            for dur in RAMP_DURATIONS
+        ):
+            return run + 1
+    return 0
+
+_run_idx          = _find_resume_run()
 OBJECT_NAME       = OBJECTS[_obj_idx]
 _current_ramp_dur = RAMP_DURATIONS[0]
 
