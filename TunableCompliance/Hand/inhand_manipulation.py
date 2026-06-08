25f81345c2398e7773de924726f67ffcf492fbef
vigno0405 <lorenzo.vignoli0405@gmail.com>
2026-06-08 11:07:39 +0200

    Update 2026-06-08

--- TunableCompliance/Hand/inhand_manipulation.py
+++ TunableCompliance/Hand/inhand_manipulation.py
@@ -59,7 +59,7 @@ import rtde_control
 # =============================================================================
 # Collected data
 # =============================================================================
-COLLECTED_DATA = False
+COLLECTED_DATA = True
 
 B_RETURN  = K_RETURN * (B_ROT / K_ROT if K_ROT else 0.0)
 LOG_EVERY = max(1, int(CONTROL_FREQUENCY / 30))
